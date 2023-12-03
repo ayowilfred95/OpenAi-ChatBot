d52c45318281c948a805ac1fd0dfecfe978d15ef
Ayowilfred <38457345+ayowilfred95@users.noreply.github.com>
2023-12-03 16:24:14 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { NextPage, NextPageContext } from 'next';
 import { AppProps } from 'next/app';
+import RootLayout from './layouts';  // Import your custom layout
+import Navbar from './components/Navbar';  // Import your Navbar component
+import './globals.css';
 
 const Page: NextPage<AppProps> = ({ Component, pageProps }) => {
   // Your component logic here
-  return <Component {...pageProps} />;
+  return (
+    <RootLayout>
+      <Navbar />
+      <Component {...pageProps} />
+    </RootLayout>
+  );
 };
 
 export default Page;
